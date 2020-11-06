b5eef3e9f08faca0e1f21235fe6845d02b2020a4
SeungCheol <ssc6839@gmail.com>
2020-11-06 17:39:29 +0900

    Recursion

---
+++ Category/Recursion/10872_팩토리얼.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int Factorial(int n) {
+	if (n == 0) return 1;
+	return n * Factorial(n - 1);
+}
+
+int main(void) {
+	int n;
+
+	cin >> n;
+	cout << Factorial(n);
+	return 0;
+}
